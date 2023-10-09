627bf586fc13ca96b6180a35ce2ad72606e03edd
edier@LAPTOP-H0ND0C88 <ediermartinezmartinez@gmail.com>
2023-10-08 22:08:06 -0600

    organismos y css

---
+++ src/components/organismos/informacion.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Imagen from '../atoms/Imagen';
+import Titulo from '../atoms/Titulo';
+import Button from '../atoms/Boton';
+import Parrafo from '../atoms/Parrafo';
+import '../../css/minecraft.css';
+
+export default function InfoMinecraft(props) {
+    return (
+      <div className="card">
+        <Imagen className='' logosrc={props.imagenSrc} logoWidth={props.logoWidth} logoHeight={props.logoHeight} />
+        <div className="title">
+          {props.contenido.map((item, index) => (
+            <div key={index}>
+              <Titulo className='' titulo={item.titulo} />
+              <Parrafo className='' parrafo={item.parrafo} />
+            </div>
+          ))}
+          <Button className='' text="Comprar" onClick={() => console.log("Botón clickeado")} />
+        </div>
+      </div>
+    );
+  }
